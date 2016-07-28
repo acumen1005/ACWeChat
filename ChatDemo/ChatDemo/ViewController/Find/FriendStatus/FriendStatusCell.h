e7122147971c65efe75b518221dfa9e04d877167
isacumen <407660109@qq.com>
2016-07-28 19:18:47 +0800

    update file struct

---
+++ ChatDemo/ChatDemo/ViewController/Find/FriendStatus/FriendStatusCell.h
@@ -0,0 +1,13 @@
+//
+//  FriendStatusCell.h
+//  ChatDemo
+//
+//  Created by acumen on 16/7/28.
+//  Copyright © 2016年 acumen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FriendStatusCell : UITableViewCell
+
+@end
---
+++ ChatDemo/ChatDemo/ViewController/Find/FriendStatus/FriendStatusCell.m
@@ -0,0 +1,23 @@
+//
+//  FriendStatusCell.m
+//  ChatDemo
+//
+//  Created by acumen on 16/7/28.
+//  Copyright © 2016年 acumen. All rights reserved.
+//
+
+#import "FriendStatusCell.h"
+
+@implementation FriendStatusCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
